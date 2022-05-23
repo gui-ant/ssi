b16e83a2ac025aa80573e17fe50bc80bda9971f1
Guilherme Antunes <antunes.gfa@gmail.com>
2022-05-23 01:50:46 +0100

    Update rest-api.js

--- rest-api.js
+++ rest-api.js
@@ -4,26 +4,13 @@ import * as db from "./functions.js";
 import jwt from "jsonwebtoken";
 const API_KEY = "5599299BD5284E2BB7F3B69CD568F"; // 256-bit WEP Key
 
-/**
+/********************************
+ *
  * REST API Routes
- */
+ *
+ *******************************/
 
-// GET /user
-app.get("/user", function (req, res) {
-  res.json(db.getAllUsers());
-});
-
-// GET /user/:id
-app.get("/user/:id", function (req, res) {
-  let u = db.getUser(req.params.id);
-  if (u) {
-    res.json(u);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-// POST /user
+// CREATE a user with request body data
 app.post("/user", function (req, res) {
   let u = db.createUser(req.body);
   if (u) {
@@ -33,7 +20,17 @@ app.post("/user", function (req, res) {
   }
 });
 
-// PUT /user/:id
+// READ user info by id
+app.get("/user/:id", function (req, res) {
+  let u = db.getUser(req.params.id);
+  if (u) {
+    res.json(u);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// UPDATE user with request body dada
 app.put("/user/:id", function (req, res) {
   let u = db.getUser(req.params.id);
   if (u) {
@@ -44,7 +41,7 @@ app.put("/user/:id", function (req, res) {
   }
 });
 
-// DELETE /user/:id
+// DELETE user by id
 app.delete("/user/:id", function (req, res) {
   let u = db.getUser(req.params.id);
   if (u) {
@@ -55,22 +52,13 @@ app.delete("/user/:id", function (req, res) {
   }
 });
 
-/**
- * JWT Protected Routes
- */
+/****************************
+ *
+ * JWT Protected route
+ *
+ ****************************/
 
-// POST /login
-app.post("/login", function (req, res) {
-  const user = db.getUser(req.body.id);
-  if (user) {
-    const token = jwt.sign({ user }, API_KEY);
-    res.json({ token: token });
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-// GET /user/:id
+// READ user info by id
 app.get("/jwt/user/:id", ensureToken, function (req, res) {
   jwt.verify(req.token, API_KEY, function (err, data) {
     if (err || data.user.id !== req.params.id) {
@@ -81,6 +69,18 @@ app.get("/jwt/user/:id", ensureToken, function (req, res) {
   });
 });
 
+// Login / Token generate
+app.post("/login", function (req, res) {
+  const user = db.getUser(req.body.id);
+  if (user) {
+    const token = jwt.sign({ user }, API_KEY);
+    res.json({ token: token });
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// Middleware
 function ensureToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   if (typeof authHeader !== "undefined") {
